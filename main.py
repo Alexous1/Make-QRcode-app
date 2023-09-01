9e4786dfbb887a09f139737f1ee0846d53aef19e
Alexous1 <ademoustier69@gmail.com>
2023-09-01 21:10:07 +0200

    new version of information code

--- main.py
+++ main.py
@@ -19,11 +19,11 @@ class Main():
         self.root.geometry('250x215')
         self.img = 'rien'
 
-        # Creation de la fonction qui va recuperer le lien de la destination du fichier
+        # Create a function to have the destination of the file
         def OpenDirectory():
             self.file_path = filedialog.askdirectory()
 
-        # Creation de la fonction qui va creer le QRcode
+        # Create a function to create the QRcode and save it
         def CreateQRcode():
             qr = qrcode.QRCode(
                 version=random.randint(1, 10),
@@ -32,10 +32,11 @@ class Main():
                 border=4,
             )
 
-
+            # add the link to the Qrcode
             qr.add_data(self.entree.get())
             qr.make(fit=True)
 
+            # Save the Qrcode to the right destination
             self.img = qr.make_image(fill_color="black", back_color="white")
             self.img.save(self.file_path + '/' + self.title.get())
 
@@ -43,27 +44,27 @@ class Main():
 
             self.imageLue.show()
 
-        # Creation du label pour expliquer qu'il faut mettre un lien
+        # Create a label to explain the need a link
         self.explain = customtkinter.CTkLabel(master=self.root, text='Entrer un lien')
         self.explain.pack()
 
-        # Creation d'une entry pour entrer le lien
+        # Create an entry to have the link of the Qrcode
         self.entree = customtkinter.CTkEntry(master=self.root, width=200)
         self.entree.pack()
 
-        # # Creation d'un label expliquer qu'il faut bien mettre son expansion
+        # Create a label to explain it's very important to give the extension of the file with the name of the file
         self.titleLabel = customtkinter.CTkLabel(master=self.root, text="donner un nom (avec son extension)")
         self.titleLabel.pack()
 
-        # Creation d'une entry pour entrer le nom du fichier
+        # Create ann entry to give a name of the Qrcode file
         self.title = customtkinter.CTkEntry(master=self.root)
         self.title.pack()
 
-        # Creation du boutton pour choisi la location du QRcode
+        # Create a button to choose the destination of the file
         self.button = customtkinter.CTkButton(master=self.root, text="Choisir un endroit", command=OpenDirectory)
         self.button.pack(pady=10)
 
-        # Creation du bouton pour enregistrer le QRcode
+        # Create a button to create and save the Qrcode
         self.finish = customtkinter.CTkButton(master=self.root, text="créer", command=CreateQRcode)
         self.finish.pack()
 
