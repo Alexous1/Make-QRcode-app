c222def65b176452c08d363d28dd026a60b75147
Alexous1 <ademoustier69@gmail.com>
2023-09-01 16:47:06 +0200

    new version

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from tkinter import filedialog
+import customtkinter
 import qrcode
 import random
 from PIL import Image
@@ -8,12 +9,14 @@ class Main():
 
     def __init__(self):
 
+        customtkinter.set_appearance_mode("dark")
+        customtkinter.set_default_color_theme("dark-blue")
+
+
         # Creation de la fenetre
-        self.fenetre = Tk()
-        self.fenetre.title("Create a qrcode")
-        self.bg = "#25ebf1"
-        self.fenetre.geometry('300x275')
-        self.fenetre.config(bg=self.bg)
+        self.root = customtkinter.CTk()
+        self.root.title("Create a qrcode")
+        self.root.geometry('250x215')
         self.img = 'rien'
 
         # Creation de la fonction qui va recuperer le lien de la destination du fichier
@@ -25,9 +28,11 @@ class Main():
             qr = qrcode.QRCode(
                 version=random.randint(1, 10),
                 error_correction=qrcode.constants.ERROR_CORRECT_L,
-                box_size=random.randint(1, 20),
+                box_size=10,
                 border=4,
             )
+
+
             qr.add_data(self.entree.get())
             qr.make(fit=True)
 
@@ -38,39 +43,30 @@ class Main():
 
             self.imageLue.show()
 
-        # Creation du premier frame qui contient le lien ainsi que le label pour expliquer
-        self.frame_lien = Frame(self.fenetre, bg=self.bg)
-        self.frame_lien.pack(pady=10)
-
-        # Creation du deuxième frame qui contient le nom du fichier ainsi que la location du fichier
-        self.frame_stop = Frame(self.fenetre, bg=self.bg)
-        self.frame_stop.pack(pady=10)
-
         # Creation du label pour expliquer qu'il faut mettre un lien
-        self.explain = Label(self.frame_lien, text='Entrer un lien', bg=self.bg, fg='black')
-        self.explain.pack(pady=2.5)
+        self.explain = customtkinter.CTkLabel(master=self.root, text='Entrer un lien')
+        self.explain.pack()
 
         # Creation d'une entry pour entrer le lien
-        self.entree = Entry(self.frame_lien, width=30, relief=GROOVE, bd=0, bg='white', fg='black')
+        self.entree = customtkinter.CTkEntry(master=self.root, width=200)
         self.entree.pack()
 
         # # Creation d'un label expliquer qu'il faut bien mettre son expansion
-        self.titleLabel = Label(self.frame_stop, text="donner un nom (avec son extension)", bg=self.bg, fg='black')
+        self.titleLabel = customtkinter.CTkLabel(master=self.root, text="donner un nom (avec son extension)")
         self.titleLabel.pack()
 
         # Creation d'une entry pour entrer le nom du fichier
-        self.title = Entry(self.frame_stop, bd=0, bg='white', fg='black')
+        self.title = customtkinter.CTkEntry(master=self.root)
         self.title.pack()
 
         # Creation du boutton pour choisi la location du QRcode
-        self.button = Button(self.frame_stop, text="Choisir un endroit", bd=0, command=OpenDirectory)
-        self.button.pack(pady=1.5)
+        self.button = customtkinter.CTkButton(master=self.root, text="Choisir un endroit", command=OpenDirectory)
+        self.button.pack(pady=10)
 
         # Creation du bouton pour enregistrer le QRcode
-        self.finish = Button(self.frame_stop, text="créer", command=CreateQRcode, bd=0)
-        self.finish.pack(pady=20)
+        self.finish = customtkinter.CTkButton(master=self.root, text="créer", command=CreateQRcode)
+        self.finish.pack()
 
-
-        self.fenetre.mainloop()
+        self.root.mainloop()
 
 Main()
